54e872c808fa1e43cefaab04e0c7cdb7550cece5
biccsdev <biccsdev@gmail.com>
2023-05-15 17:16:25 -0600

    working on apartment details

--- pages/depasBaja.js
+++ pages/depasBaja.js
@@ -6,6 +6,7 @@ import NavBar from '../components/navbar'
 import ApartmentCard from '../components/cityApartments/cityApartmentCard';
 
 const depaCaboA = {
+    _id: "fsadihw4k3hrj3",
     title: "Departamento en Los Cabos",
     description: "Increíble condominio muy cerca del centro. Rooftop con asador y alberca donde podrá disfrutar de la hermosa vista.Este condominio tiene todo lo necesario para una estancia confortable. Tendrás las mejores vacaciones aquí",
     thumbnail: "/apartmentPictures/losCabos/A/highlight.png",
@@ -43,11 +44,58 @@ const depaCaboA = {
             description: "Cocina Integral"
         },
     ],
-    map: "",
+    map: "https://goo.gl/maps/VyMW5x4JBtTgrU4XA",
     unAvailableDays: [],
-    price: 0,
+    price: 2500,
     reviews: [{}],
-    rules: [""]
+    rules: ["Check-In despues de las 3:00 PM", "Checkout antes de las 12:00 PM", "6 Invitados como maximo"]
+
+}
+
+const depaCaboB = {
+    _id: "dlfj4jnkjnt4k",
+    title: "Fascinante Departamento en Los Cabos",
+    description: "Condo 'Refugio el Paraiso', un lugar hermoso, espacioso y relajante con vistas panoramicas.",
+    thumbnail: "/apartmentPictures/losCabos/B/highlight.png",
+    photos: [],
+    highlights: [
+        {
+            title: "Area para trabajo",
+            description: "El condominio cuenta con un área cómoda para trabajar, además cuenta con excelente conexión privada a internet."
+        },
+        {
+            title: "Check-In Privado",
+            description: "Haz check-in privado utilizando la caja de seguridad"
+        },
+        {
+            title: "Excelente Ubicacion",
+            description: "condominio con una gran locación, ubicado a 1 cuadra de la playa."
+        },
+    ],
+    about: "¡Apartamento a estrenar en Torre BNI, ubicado en el corazón de la Zona Dorada! Cerca de plazas comerciales, hospitales, campos de golf y la mejor playa al otro lado de la calle! Hermosas vistas al mar desde la azotea de la torre. Totalmente equipado, con aire acondicionado, lavadora-secadora, cocina completa con todos los utensilios de cocina, wifi rápido, TV de 65 pulgadas y más. Un espacio de estacionamiento, piscina en la azotea y gimnasio. El espacio Cuenta con 2 habitaciones, la principal con cama queen size y baño dentro de la habitación, la segunda tiene 2 camas individuales y baño al salir de la habitación. Acceso de invitado Cuando llegues a la torre habrá un vigilante que ya estará avisado de tu llegada, te dará acceso y de igual forma te apoyará con la entrega de llaves en caso de ser necesario.",
+    amenities: [
+        {
+            url: "",
+            description: "3 Camas, 1 Matrimonial y 2 individuales"
+        },
+        {
+            url: "",
+            description: "Vista al mar"
+        },
+        {
+            url: "",
+            description: "Wifi"
+        },
+        {
+            url: "",
+            description: "Cocina Integral"
+        },
+    ],
+    map: "https://goo.gl/maps/VyMW5x4JBtTgrU4XA",
+    unAvailableDays: [],
+    price: 2500,
+    reviews: [{}],
+    rules: ["Check-In despues de las 3:00 PM", "Checkout antes de las 12:00 PM", "6 Invitados como maximo"]
 
 }
 
@@ -61,8 +109,8 @@ export default function DepasBaja() {
             </Head>
             <NavBar />
             <main className=" w-full h-full bg-slate-200">
-                <ApartmentCard />
-                <ApartmentCard />
+                <ApartmentCard data={depaCaboA} />
+                <ApartmentCard data={depaCaboB} />
             </main>
         </div>
     )
