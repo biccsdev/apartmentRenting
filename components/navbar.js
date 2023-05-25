6f9025b1fc9925996041f0775e9c42b5d57cca67
biccsdev <biccsdev@gmail.com>
2023-05-24 20:29:06 -0600

    laying foundation for auth implementation

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 function NavBar() {
+    const [logged, setLogged] = useState(null);
+    const [userId, setUserId] = useState(null);
     return (
         <nav className="flex items-center justify-between flex-wrap bg-slate-100 z-10 fixed w-full">
             <div className="flex items-center flex-shrink-0 text-white">
@@ -9,9 +12,14 @@ function NavBar() {
                 </Link>
             </div>
             <div className="flex items-center p-4">
-                <Link className="bg-orange-400 hover:bg-gray-100 text-slate-100 font-semibold py-2 px-4 border border-orange-700 rounded shadow" href="/login">
-                    Login
-                </Link>
+                {!logged && (
+                    <Link className="bg-orange-400 hover:bg-gray-100 text-slate-100 font-semibold py-2 px-4 border border-orange-700 rounded shadow" href="/login">
+                        Login
+                    </Link>
+                )}
+                {logged && (
+                    <Link className="bg-orange-400 hover:bg-gray-100 text-slate-100 font-semibold py-2 px-4 border border-orange-700 rounded shadow" href={`/user/${userId}`}>Perfil</Link>
+                )}
             </div>
         </nav>
     );
--- pages/user/[user].js
+++ pages/user/[user].js
@@ -1,5 +1,61 @@
-export default function User() {
+import { useState } from 'react';
+
+export default function ProfileComponent() {
     const router = useRouter();
     const data = router.query.apartment;
     console.log(data);
-}
+    const [showCode, setShowCode] = useState(false);
+
+    const handleShowCode = () => {
+        setShowCode(true);
+    };
+
+    return (
+        <div className="max-w-md mx-auto bg-white rounded p-4 shadow">
+            <h1 className="text-2xl font-bold mb-4">Perfil</h1>
+
+            <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">Nombre Completo</h1>
+                <h1 className="text-gray-600">John Doe</h1>
+            </div>
+
+            <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">Correo</h1>
+                <h1 className="text-gray-600">johndoe@example.com</h1>
+            </div>
+
+            <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">Teléfono</h1>
+                <h1 className="text-gray-600">123-456-7890</h1>
+            </div>
+
+            <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">Rentas Activas</h1>
+                <div className="p-4 bg-gray-100 rounded">
+                    <h1 className="text-lg font-bold mb-2">Clave Caja de Llaves</h1>
+                    <p className="text-gray-600 mb-4">Description</p>
+                    {showCode ? (
+                        <p className="text-2xl font-bold">55555</p>
+                    ) : (
+                        <p className="text-2xl font-bold">####</p>
+                    )}
+                    <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+                        onClick={handleShowCode}
+                    >
+                        Show Code
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                <h1 className="text-xl font-bold mb-2">Rentas Pasadas</h1>
+                <div className="p-4 bg-gray-100 rounded">
+                    <h1 className="text-lg font-bold mb-2">Title</h1>
+                    <h3 className="text-gray-600">Date</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
