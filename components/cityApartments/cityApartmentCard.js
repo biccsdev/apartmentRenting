b6229e72a672977027c5d801b8ce52b2fb53cf8b
biccsdev <biccsdev@gmail.com>
2023-11-14 22:12:38 -0600

    refactor cityApartmentCard component

--- components/cityApartments/cityApartmentCard.js
+++ components/cityApartments/cityApartmentCard.js
@@ -2,33 +2,25 @@ import Link from 'next/link';
 
 function ApartmentCard(data) {
     const { _id, title, description, thumbnail, location } = data.data;
-    return (
-        <div className='bg-transparent pt-10 p-8 md:w-2/5'>
-            {(location === "LosCabosA") && (
-                <div className='rounded p-3 shadow-lg overflow-hidden md:w-1/2 md:m-0 md:m-auto'>
-                    <img className='' src={'/apartmentPictures/losCabos/A/highlight.png'} alt='view of los cabos' />
-                </div>
-            )}
-            {(location === "LosCabosB") && (
-                <div className='rounded p-3 shadow-lg overflow-hidden md:w-1/2 md:m-0 md:m-auto'>
-                    <img src={'/apartmentPictures/losCabos/B/highlight.png'} alt='view of los cabos' />
-                </div>
-            )}
-            {(location === "MazatlanA") && (
-                <div className='rounded p-3 shadow-lg overflow-hidden md:w-1/2 md:m-0 md:m-auto'>
-                    <img src={'/apartmentPictures/mazatlan/A/highlight.png'} alt='view of los cabos' />
-                </div>
-            )}
-            {(location === "MazatlanB") && (
-                <div className='rounded p-3 shadow-lg overflow-hidden md:w-1/2 md:m-0 md:m-auto'>
-                    <img src={'/apartmentPictures/mazatlan/B/highlight.png'} alt='view of los cabos' />
-                </div>
-            )}
+    console.log(location)
 
-            <div className='flex p-3'><Link className='bg-orange-400 p-2 rounded shadow-lg ' href={`/apartment/${_id}`} >Detalles</Link>
-                <h3 className='text-zinc-500 pl-4 pt-2'>{title}</h3>
+    const imageSrc = `/apartmentPictures/${location.slice(0, location.length - 1).toLowerCase()}/${location.slice(location.length - 1, location.length)}/highlight.png`;
+
+    return (
+        <div className='bg-white pt-10 p-8 md:w-2/5 md:m-auto'>
+            <div className='rounded p-3 shadow-lg overflow-hidden md:w-1/2 md:m-auto'>
+                <img className='w-full h-auto rounded-md' src={imageSrc} alt={`View of ${location}`} />
             </div>
-            <p className='text-zinc-800 p-3'>{description}</p>
+
+            <div className='flex items-center mt-4'>
+                <Link href={`/apartment/${_id}`}>
+                    <div className='bg-orange-500 px-4 py-2 rounded text-white hover:bg-orange-600 transition duration-300 ease-in-out'>
+                        Detalles
+                    </div>
+                </Link>
+                <h3 className='text-xl text-gray-800 ml-4'>{title}</h3>
+            </div>
+            <p className='text-gray-700 mt-2'>{description}</p>
         </div>
     );
 }
